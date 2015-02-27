06389bfe5d280a32a732fe4764c3b92ccaecd063
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-02-27 01:56:38 +0000

    fix nose-traceback-info inclusion per greg
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.recipes.zodb/trunk@60410 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -12,27 +12,26 @@ entry_points = {
 }
 
 setup(
-    name = 'nti.recipes.zodb',
-    version = VERSION,
-    author = 'Jason Madden',
-    author_email = 'open-source@nextthought.com',
-    description = "zc.buildout recipes for RelStorage and ZEO",
-    long_description = codecs.open('README.rst', encoding='utf-8').read(),
-    license = 'Proprietary',
-    keywords = 'buildout relstorage ZEO',
-    #url = 'https://github.com/NextThought/nti.nose_traceback_info',
-    classifiers = [
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
+	name = 'nti.recipes.zodb',
+	version = VERSION,
+	author = 'Jason Madden',
+	author_email = 'open-source@nextthought.com',
+	description = "zc.buildout recipes for RelStorage and ZEO",
+	long_description = codecs.open('README.rst', encoding='utf-8').read(),
+	license = 'Proprietary',
+	keywords = 'buildout relstorage ZEO',
+	classifiers = [
+		'Development Status :: 5 - Production/Stable',
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'Operating System :: OS Independent',
 		'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 2.7',
 		'Programming Language :: Python :: 3',
 		'Programming Language :: Python :: 3.3',
-        'Topic :: Software Development :: Testing'
+		'Topic :: Software Development :: Testing'
 		'Framework :: Buildout',
-        ],
+	],
 	packages=find_packages('src'),
 	package_dir={'': 'src'},
 	namespace_packages=['nti', 'nti.recipes'],
